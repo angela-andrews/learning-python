9bbb5111ee28e8972f7df55ddd0d85bfa98722ef
Angela Andrews <aandrews@swarthmore.edu>
2019-11-06 11:10:02 -0500

    Working on branches and functions

---
+++ ex34.py
@@ -0,0 +1,16 @@
+# Accessing Elements of lists
+
+animals = ['bear', 'python3.7','peacock' , 'kangaroo', 'whale','platypus']
+
+# Cardinal (like "the animal at 1" is 1)
+# ordinal (1st, 2nd, 3rd) subtact 1
+
+
+print(f"The animal at 1 is: {animals[1]}")
+print(f"The 3rd animal is: {animals[2]} ")
+print(f"The 1st animal is: {animals[0]} ")
+print(f"The animal at 3 is: {animals[3]}")
+print(f"The 5th animal is: {animals[4]}")
+print(f"The animal at 2 is: {animals[2]}")
+print(f"The 6th animal is: {animals[5]}")
+print(f"The animal at 4 is: {animals[4]}")
